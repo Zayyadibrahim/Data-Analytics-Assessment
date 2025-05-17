196a3ed583d310d0b29a7855c157e1db606f427a
Zayyad Ibrahim <31861239+Zayyadibrahim@users.noreply.github.com>
2025-05-17 17:40:02 +0100

    Update Assessment_Q1 .sql

--- Assessment_Q1 .sql
+++ Assessment_Q1 .sql
@@ -6,7 +6,7 @@ SELECT
     CONCAT(users.first_name, ' ', users.last_name) AS name,
     COUNT(DISTINCT CASE WHEN plan.is_regular_savings = 1 THEN plan.id END) AS savings_count,
     COUNT(DISTINCT CASE WHEN plan.is_a_fund = 1 THEN plan.id END) AS investment_count,
-    ROUND(SUM(save.confirmed_amount) / 100.0, 0) AS total_deposits_naira -- figures are converted from Kobo to Naira.
+    ROUND(SUM(save.confirmed_amount) / 100, 0) AS total_deposits_naira -- figures are converted from Kobo to Naira.
 FROM users_customuser users
 JOIN plans_plan plan ON users.id = plan.owner_id
 JOIN savings_savingsaccount save ON plan.id = save.plan_id
